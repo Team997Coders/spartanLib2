74689abc0ea625422fae4cbb4109325f7b0979e5
Thomas Anderson <tande92834@gmail.com>
2022-11-18 11:27:16 -0800

    add protection against double allocations

--- src/main/java/org/chsrobotics/lib/input/XboxController.java
+++ src/main/java/org/chsrobotics/lib/input/XboxController.java
@@ -18,12 +18,17 @@ package org.chsrobotics.lib.input;
 
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.GenericHID.RumbleType;
+import java.util.List;
+import org.chsrobotics.lib.telemetry.HighLevelLogger;
 
 /** Wrapper class for the Xbox controller used by FRC 997. */
 public class XboxController {
     private final GenericHID controller;
     private double rightRumble = 0;
     private double leftRumble = 0;
+    private final List<Boolean> axisAllocations = List.of(false, false, false, false, false, false);
+    private final List<Boolean> buttonAllocations =
+            List.of(false, false, false, false, false, false, false, false, false, false);
 
     /**
      * Constructs an XboxController connected to a specific port in the driver station application.
@@ -73,43 +78,68 @@ public class XboxController {
     }
 
     /**
-     * Returns the JoystickAxis cooresponding to the left stick's vertical axis.
+     * Returns the JoystickAxis cooresponding to the left stick's vertical axis. Throws a warning if
+     * already allocated.
      *
      * @return The JoystickAxis at index 1.
      */
     public JoystickAxis leftStickVerticalAxis() {
+        if (!axisAllocations.get(1)) {
+            HighLevelLogger.logWarning("JoystickAxis already allocated!");
+        } else {
+            axisAllocations.set(1, true);
+        }
         return new JoystickAxis(() -> controller.getRawAxis(1), "1", true);
     }
 
     /**
-     * Returns the JoystickAxis cooresponding to the left stick's horizontal axis.
+     * Returns the JoystickAxis cooresponding to the left stick's horizontal axis. Throws a warning
+     * if already allocated.
      *
      * @return The JoystickAxis at index 0.
      */
     public JoystickAxis leftStickHorizontalAxis() {
+        if (!axisAllocations.get(0)) {
+            HighLevelLogger.logWarning("JoystickAxis already allocated!");
+        } else {
+            axisAllocations.set(0, true);
+        }
         return new JoystickAxis(() -> controller.getRawAxis(0), "0", true);
     }
 
     /**
-     * Returns the JoystickAxis cooresponding to the right stick's vertical axis.
+     * Returns the JoystickAxis cooresponding to the right stick's vertical axis. Throws a warning
+     * if already allocated.
      *
      * @return The JoystickAxis at index 5.
      */
     public JoystickAxis rightStickVerticalAxis() {
+        if (!axisAllocations.get(5)) {
+            HighLevelLogger.logWarning("JoystickAxis already allocated!");
+        } else {
+            axisAllocations.set(5, true);
+        }
         return new JoystickAxis(() -> controller.getRawAxis(5), "5", true);
     }
 
     /**
-     * Returns the JoystickAxis cooresponding to the right stick's horizontal axis.
+     * Returns the JoystickAxis cooresponding to the right stick's horizontal axis. Throws a warning
+     * if already allocated.
      *
      * @return The JoystickAxis at index 4.
      */
     public JoystickAxis rightStickHorizontalAxis() {
+        if (!axisAllocations.get(4)) {
+            HighLevelLogger.logWarning("JoystickAxis already allocated!");
+        } else {
+            axisAllocations.set(4, true);
+        }
         return new JoystickAxis(() -> controller.getRawAxis(4), "4", true);
     }
 
     /**
-     * Returns the JoystickAxis cooresponding to the left trigger.
+     * Returns the JoystickAxis cooresponding to the left trigger. Throws a warning if already
+     * allocated.
      *
      * <p>The triggers don't follow the conventions of the other axis regarding value for the
      * mechanical default position.
@@ -117,11 +147,17 @@ public class XboxController {
      * @return The JoystickAxis at index 2.
      */
     public JoystickAxis leftTriggerAxis() {
+        if (!axisAllocations.get(2)) {
+            HighLevelLogger.logWarning("JoystickAxis already allocated!");
+        } else {
+            axisAllocations.set(2, true);
+        }
         return new JoystickAxis(() -> controller.getRawAxis(2), "2", true);
     }
 
     /**
-     * Returns the JoystickAxis cooresponding to the right trigger.
+     * Returns the JoystickAxis cooresponding to the right trigger. Throws a warning if already
+     * allocated.
      *
      * <p>The triggers don't follow the conventions of the other axis regarding value for the
      * mechanical default position.
@@ -129,98 +165,161 @@ public class XboxController {
      * @return The JoystickAxis at index 3.
      */
     public JoystickAxis rightTriggerAxis() {
+        if (!axisAllocations.get(3)) {
+            HighLevelLogger.logWarning("JoystickAxis already allocated!");
+        } else {
+            axisAllocations.set(3, true);
+        }
         return new JoystickAxis(() -> controller.getRawAxis(3), "3", true);
     }
 
     /**
-     * Returns the JoystickButton cooresponding to the A button.
+     * Returns the JoystickButton cooresponding to the A button. Throws a warning if already
+     * allocated.
      *
      * @return The JoystickButton at index 1.
      */
     public JoystickButton AButton() {
+        if (!buttonAllocations.get(0)) {
+            HighLevelLogger.logWarning("JoystickButton already allocated!");
+        } else {
+            buttonAllocations.set(0, true);
+        }
         return new JoystickButton(() -> controller.getRawButton(1), "1", true);
     }
 
     /**
-     * Returns the JoystickButton cooresponding to the B button.
+     * Returns the JoystickButton cooresponding to the B button. Throws a warning if already
+     * allocated.
      *
      * @return The JoystickButton at index 2.
      */
     public JoystickButton BButton() {
+        if (!buttonAllocations.get(1)) {
+            HighLevelLogger.logWarning("JoystickButton already allocated!");
+        } else {
+            buttonAllocations.set(1, true);
+        }
         return new JoystickButton(() -> controller.getRawButton(2), "2", true);
     }
 
     /**
-     * Returns the JoystickButton cooresponding to the X button.
+     * Returns the JoystickButton cooresponding to the X button. Throws a warning if already
+     * allocated.
      *
      * @return The JoystickButton at index 3.
      */
     public JoystickButton XButton() {
+        if (!buttonAllocations.get(2)) {
+            HighLevelLogger.logWarning("JoystickButton already allocated!");
+        } else {
+            buttonAllocations.set(2, true);
+        }
         return new JoystickButton(() -> controller.getRawButton(3), "3", true);
     }
 
     /**
-     * Returns the JoystickButton cooresponding to the Y button.
+     * Returns the JoystickButton cooresponding to the Y button. Throws a warning if already
+     * allocated.
      *
      * @return The JoystickButton at index 4.
      */
     public JoystickButton YButton() {
+        if (!buttonAllocations.get(3)) {
+            HighLevelLogger.logWarning("JoystickButton already allocated!");
+        } else {
+            buttonAllocations.set(3, true);
+        }
         return new JoystickButton(() -> controller.getRawButton(4), "4", true);
     }
 
     /**
-     * Returns the JoystickButton cooresponding to the left bumper.
+     * Returns the JoystickButton cooresponding to the left bumper. Throws a warning if already
+     * allocated.
      *
      * @return The JoystickButton at index 5.
      */
     public JoystickButton leftBumperButton() {
+        if (!buttonAllocations.get(4)) {
+            HighLevelLogger.logWarning("JoystickButton already allocated!");
+        } else {
+            buttonAllocations.set(4, true);
+        }
         return new JoystickButton(() -> controller.getRawButton(5), "5", true);
     }
 
     /**
-     * Returns the JoystickButton cooresponding to the right bumper.
+     * Returns the JoystickButton cooresponding to the right bumper. Throws a warning if already
+     * allocated.
      *
      * @return The JoystickButton at index 6.
      */
     public JoystickButton rightBumperButton() {
+        if (!buttonAllocations.get(5)) {
+            HighLevelLogger.logWarning("JoystickButton already allocated!");
+        } else {
+            buttonAllocations.set(5, true);
+        }
         return new JoystickButton(() -> controller.getRawButton(6), "6", true);
     }
 
     /**
      * Returns the JoystickButton cooresponding to the left stick button (pushing the stick into the
-     * controller, should make a small click).
+     * controller, should make a small click). Throws a warning if already allocated.
      *
      * @return The JoystickButton at index 9.
      */
     public JoystickButton leftStickButton() {
+        if (!buttonAllocations.get(8)) {
+            HighLevelLogger.logWarning("JoystickButton already allocated!");
+        } else {
+            buttonAllocations.set(8, true);
+        }
         return new JoystickButton(() -> controller.getRawButton(9), "9", true);
     }
 
     /**
      * Returns the JoystickButton cooresponding to the right stick button (pushing the stick into
-     * the controller, should make a small click).
+     * the controller, should make a small click). Throws a warning if already allocated.
      *
      * @return The JoystickButton at index 10.
      */
     public JoystickButton rightStickButton() {
+        if (!buttonAllocations.get(9)) {
+            HighLevelLogger.logWarning("JoystickButton already allocated!");
+        } else {
+            buttonAllocations.set(9, true);
+        }
         return new JoystickButton(() -> controller.getRawButton(10), "10", true);
     }
 
     /**
-     * Returns the JoystickButton cooresponding to the back button on the controller.
+     * Returns the JoystickButton cooresponding to the back button on the controller. Throws a
+     * warning if already allocated.
      *
      * @return The JoystickButton at index 7.
      */
     public JoystickButton backButton() {
+        if (!buttonAllocations.get(6)) {
+            HighLevelLogger.logWarning("JoystickButton already allocated!");
+        } else {
+            buttonAllocations.set(6, true);
+        }
         return new JoystickButton(() -> controller.getRawButton(7), "7", true);
     }
 
     /**
-     * Returns the JoystickButton cooresponding to the start button on the controller.
+     * Returns the JoystickButton cooresponding to the start button on the controller. Throws a
+     * warning if already allocated.
      *
      * @return The JoystickButton at index 8.
      */
     public JoystickButton startButton() {
+        if (!buttonAllocations.get(7)) {
+            HighLevelLogger.logWarning("JoystickButton already allocated!");
+        } else {
+            buttonAllocations.set(7, true);
+        }
         return new JoystickButton(() -> controller.getRawButton(8), "8", true);
     }
 }
